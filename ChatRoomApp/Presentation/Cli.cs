1b51e2af7509c5054660339e66643f4e6a011d80
Lior <liorhass@post.bgu.ac.il>
2018-04-02 20:01:31 +0300

    cli class

---
+++ ChatRoomApp/Presentation/Cli.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    class Cli
+    {
+        private Boolean running;
+        private ChatRoom myChatRoom;
+        private List<menuItem> menuItems;
+
+        public void showMenu()
+        {
+
+        }
+
+        private void registration()
+        {
+
+        }
+
+        private void logonLogout()
+        {
+
+        }
+
+        private void retrive10Messages()
+        {
+
+        }
+
+        private void display20Messages()
+        {
+
+        }
+
+        private void diplayAllByUser()
+        {
+
+        }
+
+        private void writeMessage()
+        {
+
+        }
+
+        private void exit()
+        {
+
+        }
+
+
+    }
+}
