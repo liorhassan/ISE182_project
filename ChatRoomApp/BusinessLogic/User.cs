0c79dc62275b2f722c379632f9118fed22cffaa3
Lior <liorhass@post.bgu.ac.il>
2018-04-02 20:22:35 +0300

    User class

---
+++ ChatRoomApp/BusinessLogic/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    class User
+    {
+        private readonly int groupID = 24;
+        private String nickname;
+
+        public String Nickname
+        {
+            get { return nickname; }
+        }
+        
+        public User(String nickname)
+        {
+            this.nickname = nickname;
+        }
+        
+        public Message (String message, String url)
+        {
+
+        }
+    }
+}
