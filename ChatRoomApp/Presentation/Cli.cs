c0d1289a87a672f78bde4a414b80cac89501a65f
Lior <liorhass@post.bgu.ac.il>
2018-04-02 22:52:21 +0300

    temp

--- ChatRoomApp/Presentation/Cli.cs
+++ ChatRoomApp/Presentation/Cli.cs
@@ -14,16 +14,41 @@ namespace Presentation
 
         public void showMenu()
         {
+            Console.Clear();
 
         }
 
         private void registration()
         {
-
+            Console.Clear();
+            Console.WriteLine("Enter your nickname");
+            String nickname = Console.ReadLine();
+            while ((nickname!="")&&(!myChatRoom.register(nickname)))
+            {
+                Console.WriteLine("Enter another nickname, or press ENTER to go back to the menu");
+                nickname = Console.ReadLine();
+            }
+         
         }
 
-        private void logonLogout()
+        private void loginLogout()
         {
+            Console.Clear();
+            if (myChatRoom.Logout())
+                Console.WriteLine("Your user logout");
+            else
+            {
+                Console.WriteLine("Enter your nickname");
+                String nickname = Console.ReadLine();
+                while ((nickname!="")&&(!myChatRoom.Login(nickname)))
+                {
+                    Console.WriteLine("User non-exist, try egain or press ENTER and go back to the menu");
+                    nickname = Console.ReadLine();
+
+                }
+
+
+            }
 
         }
 
@@ -44,7 +69,15 @@ namespace Presentation
 
         private void writeMessage()
         {
-
+            Console.Clear();
+            Console.WriteLine("Write your message");
+            String msg = Console.ReadLine();
+            while((msg!="")&& !myChatRoom.writeMessage(msg))
+            {
+                Console.WriteLine("Can't send the message, try again or press ENTER to go back to the menu");
+                msg = Console.ReadLine();
+            }
+        
         }
 
         private void exit()
