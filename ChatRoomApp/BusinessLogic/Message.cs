6b3637778e1ead324f60fd0cb95550d561822594
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2018-04-03 11:17:27 +0300

    message

--- ChatRoomApp/BusinessLogic/Message.cs
+++ ChatRoomApp/BusinessLogic/Message.cs
@@ -6,8 +6,39 @@ using System.Threading.Tasks;
 
 namespace BusinessLogic
 {
-    public class Message
+    public class Message : IMessage
     {
+        private int guid;
+        private User user;
+        private DateTime date;
+        private String MessageContent;
+        private String GroupID;
 
+        public Message (User user, String MessageContent)
+        {
+            if (!checkValidity(MessageContent))
+            {
+                throw new System.ArgumentException("Message is too long");
+            }
+            this.user = user;
+            this.GroupID = user.getID();
+            this.date = DateTime.Now;
+            this.guid = 1;
+            this.MessageContent = MessageContent;
+        }
+        private Boolean checkValidity(String content)
+        {
+            if (content.Length > 150)
+            {
+                return false;
+            }
+            return true;
+        }
+        Guid Id { get; }
+        string UserName { get; }
+        DateTime Date { get; }
+        string MessageContent { get; }
+        string GroupID { get; }
+        string ToString();
     }
 }
