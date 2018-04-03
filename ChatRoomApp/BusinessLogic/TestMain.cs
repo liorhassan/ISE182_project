799d5d44edccd7900ef2ba7b748f924c85bc1683
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2018-04-03 13:50:34 +0300

    main

---
+++ ChatRoomApp/BusinessLogic/TestMain.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chatroom chatroom = new Chatroom();
+            chatroom.register("Ohad", 24);
+        }
+    }
+}
