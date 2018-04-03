ccc52bfe28171174ad00899b2449c30b81aa6194
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2018-04-03 10:59:29 +0300

    hjk

--- ChatRoomApp/BusinessLogic/Message.cs
+++ ChatRoomApp/BusinessLogic/Message.cs
@@ -6,64 +6,8 @@ using System.Threading.Tasks;
 
 namespace BusinessLogic
 {
-    public class Chatroom
+    public class Message
     {
-        private User loggedinUser;
-        private Dictionary<Message> recievedMessages;
-        private Dictionary<User> registeredUsers;
-        private String URL;
-        private messagesHandler messHandler;
-        private usersHandler usersHandler;
-        private xmlHandler xmlHandler;
-        private Logger log;
 
-        public Chatroom()
-        {
-            this.loggedinUser = null;
-            this.recievedMessages = new Dictionary<Message>();
-            this.registeredUsers = new Dictionary<User>();
-            this.URL = "url";
-            this.messHandler = new messagesHandler();
-            this.usersHandler = new usersHandler();
-            this.xmlHandler = new xmlHandler();
-            this.log = new Logger();
-        }
-
-        public boolean register(String nickname, int groupID)
-        {
-            var userOne =
-                from u in registeredUsers
-                where u.nickname == nickname && u.groupID == groupID
-                select u;
-            if (userOne != null)
-            {
-                Console.WriteLine("User already exists");
-                return false;
-            }
-
-            int loop = -1;
-            while (loop == -1)
-            {
-                var userTwo =
-                   from u in registeredUsers
-                   where u.nickname == nickname
-                   select u;
-                if (user != null)
-                {
-                    Console.WriteLine("Nickname already exists, choose another one");
-                    nickname = Console.ReadLine();
-                }
-                else
-                {
-                    loop = 0;
-                }  
-            }
-            userOne newUser = new User()
-            registeredUsers.Add()
-            return true;
-        }
-
-
-    
     }
 }
