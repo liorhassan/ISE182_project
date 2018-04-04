b70b751c405e3a816a4c504141c4a6529533a899
Lior <liorhass@post.bgu.ac.il>
2018-04-04 22:16:01 +0300

    done with user

--- ChatRoomApp/BusinessLogic/User.cs
+++ ChatRoomApp/BusinessLogic/User.cs
@@ -3,28 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MileStoneClient.CommunicationLayer;
 
 namespace BusinessLogic
 {
     class User
     {
-        private readonly int groupID = 24;
+        private readonly string groupID = "24";
         private String nickname;
 
         public String Nickname
         {
             get { return nickname; }
         }
-        
+
         public User(String nickname)
         {
             this.nickname = nickname;
         }
-        
-        public Message writeMessage (String message, String url)
+
+        public Message writeMessage(String message, String url)
         {
+            Message msg = Communication.Instance.Send(url, groupID, nickname, message);
+            return msg;
+        }
 
-
+        public List<Message> retrive10Messages(String url)
+        {
+            List<Message> messages = Communication.Instance.GetTenMessages(url);
+            return messages;
         }
     }
 }
