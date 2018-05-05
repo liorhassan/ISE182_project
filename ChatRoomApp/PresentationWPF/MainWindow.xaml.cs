215f0ae0eb47d466f218f4f7a05f49b73ccf9d15
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2018-05-05 12:20:56 +0300

    login

--- ChatRoomApp/PresentationWPF/MainWindow.xaml.cs
+++ ChatRoomApp/PresentationWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using DispatcherAndBinding;
 using System;
+using BusinessLogic;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,13 +23,65 @@ namespace PresentationWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer dispatcherTimer = new DispatcherTimer();
         ObservableObject _main = new ObservableObject();
+        private Chatroom myChatRoom;
+        private ProgramWindow pw;
         public MainWindow()
         {
             InitializeComponent();
+            myChatRoom = new Chatroom();
+            pw = new ProgramWindow(this);
+        }
+
+        private void btn_login_Click(object sender, RoutedEventArgs e)
+        {
+            String nickname = NicknameL.Text;
+            if ((nickname == ""))
+            {
+                MessageBox.Show("Please enter a nickname");
+                return;
+            }
+
+            Boolean login = myChatRoom.Login(nickname);
+            if (!login)
+            {
+                MessageBox.Show("Nickname doesn't exist");
+            }
+            else
+            {
+                StartProgram();
+            }
+        }
+
+        private void btn_register_Click(object sender, RoutedEventArgs e)
+        {
+            String nickname = NicknameR.Text;
+            if ((nickname == ""))
+            {
+                MessageBox.Show("Please enter a nickname");
+                return;
+            }
+            Boolean reg = myChatRoom.Register(nickname);
+            if (!reg)
+            {
+                MessageBox.Show("Nickname already exists, please choose another one");
+            }
+            else
+            {
+                MessageBox.Show("user " + nickname + " created succesfuly♥");
+            }
+        }
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Text = "{Binding ElementName=NicknameR, Path=Text}"
+        }
+
+        private void StartProgram()
+        {
+            pw.Show();
+            this.Hide();
         }
     }
-
-
 }
---
+++ ChatRoomApp/PresentationWPF/ProgramWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLogic;
+using DispatcherAndBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PresentationWPF
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class ProgramWindow : Window
+    {
+        ObservableObject _main = new ObservableObject();
+        MainWindow main;
+        public ProgramWindow(MainWindow main) 
+        {
+            InitializeComponent();
+            this.main = main;
+        }
+
+        private void btn_logout_Click(object sender, RoutedEventArgs e)
+        {
+            main.Show();
+            this.Close();
+        }
+    }
+}
