9b7e32e8b549afd80536a7bb267321b6c8f5dda6
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2018-05-17 16:26:33 +0300

    unittest

---
+++ ChatRoomApp/UnitTests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRegister()
+        {
+            Chatroom chatroom = new Chatroom();
+            User user = new User("user");
+            Boolean firstR = chatroom.Register(user.Nickname);
+            Assert.AreEqual(firstR, true);
+            //Boolean secondR = chatroom.Register(user.Nickname);
+           // Assert.AreEqual(secondR, false);
+        }
+
+        [TestMethod]
+        public void TestLogin()
+        {
+            Chatroom chatroom = new Chatroom();
+            User user = new User("user");
+            chatroom.Register(user.Nickname);
+            Boolean firstL = chatroom.Login("user");
+            Assert.AreEqual(firstL, true);
+            Boolean secondL = chatroom.Login("otheruser");
+            Assert.AreEqual(secondL, false);
+        }
+    }
+}
