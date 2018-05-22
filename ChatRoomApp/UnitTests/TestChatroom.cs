744b0c79d2a36f1df4eb15a6b3bdd07fb0725106
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2018-05-22 16:13:35 +0300

    final tests

---
+++ ChatRoomApp/UnitTests/TestChatroom.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommunicationLayer;
+using Persistence;
+using System.IO;
+
+namespace BusinessLogic
+{
+    public class TestChatroom
+    {
+        private static String projectpath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName;
+        private int sortType;//0-timeStamp(default), 1-nickname, 2-everything
+        private Boolean isAsc;//determain sort asc/desc
+        private int filterType;// 0-no filter(default), 1- filter by groupID, 2 - filter by user
+        private string userFilter; //user nickname to filter by
+        private string groupFilter; //groupID nickname to filter by
+        private User _loggedinUser;
+        private Dictionary<Guid, Message> recievedMessages;
+        private Dictionary<String, User> registeredUsers;
+        private readonly String URL = "http://ise172.ise.bgu.ac.il";
+        private MessagesHandler messHandler;
+        private UsersHandler usersHandler;
+
+        // a class for the chatroom
+        // constructor assigns handlers, loggers, adds content to dictionaries from handlers
+        public TestChatroom()
+        {
+            sortType = 0;
+            filterType = 0;
+            userFilter = "";
+            groupFilter = "";
+            isAsc = true;
+            messHandler = new MessagesHandler();
+            usersHandler = new UsersHandler();
+            this._loggedinUser = null;
+            recievedMessages = (Dictionary<Guid, Message>)messHandler.load();
+            if (recievedMessages == null)
+            {
+                recievedMessages = new Dictionary<Guid, Message>();
+                messHandler.save(recievedMessages);
+            }
+            registeredUsers = (Dictionary<String, User>)usersHandler.load();
+            if (registeredUsers == null)
+            {
+                registeredUsers = new Dictionary<String, User>();
+                usersHandler.save(registeredUsers);
+            }
+        }
+        // a function that registers a user
+        // doesn't do anything if a user with that nickname exists
+        // creates a new user and adds to registered users
+        public Boolean Register(String nickname, String group)
+        {
+            String key = nickname + "@" + group;
+            if (registeredUsers.ContainsKey(key))
+            {
+                return false;
+            }
+            User newUser = new User(nickname, group);
+            registeredUsers.Add(key, newUser);
+            usersHandler.save(registeredUsers);
+            return true;
+        }
+
+        // a fuction to login a user
+        // finds the user and makes the loggedinUser
+        // does nothing if the user doesn't exist
+        public Boolean Login(String nickname, String group)
+        {
+            String key = nickname + "@" + group;
+            if (registeredUsers.ContainsKey(key))
+            {
+                User user = registeredUsers[key];
+                this._loggedinUser = user;
+                return true;
+            }
+            return false;
+        }
+
+        // a fuction to logout the loggedinUser
+        // checks if the loggedinUser is not null, and then turns it to null
+        // else does nothing
+        public Boolean Logout()
+        {
+            if (this._loggedinUser != null)
+            {
+                String name = _loggedinUser.Nickname;
+                this._loggedinUser = null;
+                //ChatroomMenu.Login = false;
+                return true;
+            }
+            return false;
+        }
+
+        // a fuction to retrieve 10 messages from the server
+        // calls the fuction from the loggedinUser
+        // adds the new messages to recievedMessages
+        // returns the number of new messages added
+        public int Retrieve10Messages()
+        {
+            int c = 0;
+            foreach (IMessage m in _loggedinUser.retrive10Messages(URL))
+            {
+                if (!recievedMessages.ContainsKey(m.Id))
+                {
+                    recievedMessages.Add(m.Id, new Message(m));
+                    c++;
+                }
+            }
+            messHandler.save(recievedMessages);
+            return c;
+        }
+
+        //set filter and sort arguments givven by the presentation
+        public void SetFilterAndSort(int sortType, int filterType, Boolean isAsc, string groupFilter, string userFilter)
+        {
+            this.sortType = sortType;
+            this.filterType = filterType;
+            this.isAsc = isAsc;
+            this.groupFilter = groupFilter;
+            this.userFilter = userFilter;
+        }
+
+        //return all the messages to be shown on the message panel, filtered and sorted as needed
+        public List<String> GetAllMessages()
+        {
+            List<Message> FilteredMessages;
+            List<String> output;
+            if (filterType == 0)
+            {
+                FilteredMessages = GetMessagesByAll();
+            }
+            else if (filterType == 1)
+            {
+                FilteredMessages = GetAllByGroup();
+            }
+            else
+            {
+                FilteredMessages = GetAllByUser();
+            }
+            if (sortType == 0)
+            {
+                output = SortByTimestamp(FilteredMessages);
+            }
+            else if (sortType == 1)
+            {
+                output = SortByNickname(FilteredMessages);
+            }
+            else
+            {
+                output = SortByAll(FilteredMessages);
+            }
+            return output;
+        }
+
+        // a fuction to retrieve 20 messages from the dictionary
+        public List<Message> GetMessagesByAll()
+        {
+            var messages =
+                (from m in recievedMessages
+                 orderby m.Value.Date
+                 select m.Value);
+            return messages.ToList();
+        }
+
+
+        // a fuction to retrieve all the messages from a user
+        public List<Message> GetAllByUser()
+        {
+            var messages =
+                from m in recievedMessages
+                where m.Value.UserName == userFilter & m.Value.GroupID == groupFilter
+                orderby m.Value.Date
+                select m.Value;
+            return messages.ToList();
+        }
+
+        // a fuction to retrieve all the messages by a GID
+        public List<Message> GetAllByGroup()
+        {
+            var messages =
+                from m in recievedMessages
+                where m.Value.GroupID == groupFilter
+                orderby m.Value.Date
+                select m.Value;
+            return messages.ToList();
+        }
+
+        // a fuction to sort the messages by the timestamp
+        public List<String> SortByTimestamp(List<Message> filteredMessages)
+        {
+            if (isAsc)
+            {
+                var messages =
+                from m in filteredMessages
+                orderby m.Date ascending
+                select m.ToString();
+                return messages.ToList();
+            }
+            else
+            {
+                var messages =
+                from m in filteredMessages
+                orderby m.Date descending
+                select m.ToString();
+                return messages.ToList();
+            }
+
+        }
+
+        // a fuction to sort the messages by nickname
+        public List<String> SortByNickname(List<Message> filteredMessages)
+        {
+            if (isAsc)
+            {
+                var messages =
+                from m in filteredMessages
+                orderby m.UserName ascending
+                select m.ToString();
+                return messages.ToList();
+            }
+            else
+            {
+                var messages =
+                from m in filteredMessages
+                orderby m.UserName descending
+                select m.ToString();
+                return messages.ToList();
+            }
+        }
+
+        // a fuction to sort the messages by g_id, nickname, and timestamp
+        public List<String> SortByAll(List<Message> filteredMessages)
+        {
+
+            if (isAsc)
+            {
+                var messages =
+                from m in filteredMessages
+                orderby Convert.ToInt32(m.GroupID) ascending, m.UserName ascending, m.Date ascending
+                select m.ToString();
+                return messages.ToList();
+
+            }
+            else
+            {
+                var messages =
+                from m in filteredMessages
+                orderby Convert.ToInt32(m.GroupID) descending, m.UserName descending, m.Date descending
+                select m.ToString();
+                return messages.ToList();
+            }
+
+        }
+
+        // a fuction to write a message
+        // checks if it's valid
+        // creates the message and adds it to the dictionary
+        public int WriteMessage(String msg)
+        {
+            if (!CheckMessageValidity(msg))
+            {
+                return -1;
+            }
+            Message message = new Message(_loggedinUser.writeMessage(msg, URL));
+            recievedMessages.Add(message.Id, message);
+            messHandler.save(recievedMessages);
+            return 1;
+        }
+
+        // checks if a message is valid
+        private Boolean CheckMessageValidity(String content)
+        {
+            if (content.Length > 150)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void RestartChatroom()
+        {
+            Logout();
+            recievedMessages.Clear();
+            registeredUsers.Clear();
+        }
+    }
+}
---
+++ ChatRoomApp/UnitTests/UnitTest1old.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        Chatroom chatroom = new Chatroom();
+        User userOne = new User("userOne", "24");
+        User userTwo = new User("userTwo", "5");
+        User userThree = new User("userThree", "5");
+        User userFour = new User("userFour", "8");
+        [TestMethod()]
+        public void TestRegister()
+        {
+            chatroom.RestartChatroom();  
+            Boolean firstR = chatroom.Register(userOne.Nickname, userOne.GroupID);
+            Assert.AreEqual(firstR, true);
+            Boolean secondR = chatroom.Register(userOne.Nickname, userOne.GroupID);
+            Assert.AreEqual(secondR, false);
+           // Environment.Exit(0);
+        }
+
+        [TestMethod()]
+        public void TestLogin()
+        {
+            chatroom.RestartChatroom();
+            Console.WriteLine("after");
+            chatroom.Register(userTwo.Nickname, userTwo.GroupID);
+            Boolean firstL = chatroom.Login(userTwo.Nickname, userTwo.GroupID);
+            Assert.AreEqual(firstL, true);
+            Boolean secondL = chatroom.Login("otheruser", "4");
+            Assert.AreEqual(secondL, false);
+            chatroom.exit();
+        }
+
+
+        [TestMethod()]
+        public void TestMessage()
+        {
+            chatroom.RestartChatroom();
+            chatroom.Register(userOne.Nickname, userOne.GroupID);
+            chatroom.Login(userOne.Nickname, userOne.GroupID);
+            int first = chatroom.WriteMessage("message");
+            Assert.AreEqual(first, 1);
+            String s = new string('a', 151);
+            int second = chatroom.WriteMessage(s);
+            Assert.AreEqual(second, -1);
+        }
+
+        [TestMethod()]
+        public void TestLogout()
+        {
+            chatroom.RestartChatroom();
+            chatroom.Register(userOne.Nickname, userOne.GroupID);
+            chatroom.Login(userOne.Nickname, userOne.GroupID);
+            Boolean firstlogout = chatroom.Logout();
+            Assert.AreEqual(firstlogout, true);
+            Boolean secondlogout = chatroom.Logout();
+            Assert.AreEqual(secondlogout, false);
+            //chatroomThree.Logout();
+            //chatroom.RestartChatroom();
+            //chatroom.exit();
+        }
+
+        [TestMethod()]
+        public void TestSortByName()
+        {
+            String first = "message of userOne";
+            String second = "message of userTwo";
+            String third = "message of userThree";
+            String fourth = "message of userFour";
+            List<String> test = new List<String>(4)
+            {
+                fourth,
+                first,
+                third,
+                second  
+            };
+            chatroom.RestartChatroom();
+
+            chatroom.Register(userThree.Nickname, userThree.GroupID);
+            chatroom.Login(userThree.Nickname, userThree.GroupID);
+            chatroom.WriteMessage(third);
+            chatroom.Logout();
+
+            chatroom.Register(userFour.Nickname, userFour.GroupID);
+            chatroom.Login(userFour.Nickname, userFour.GroupID);
+            chatroom.WriteMessage(fourth);
+            chatroom.Logout();
+
+            chatroom.Register(userOne.Nickname, userOne.GroupID);
+            chatroom.Login(userOne.Nickname, userOne.GroupID);
+            chatroom.WriteMessage(first);           
+            chatroom.Logout();
+
+            chatroom.Register(userTwo.Nickname, userTwo.GroupID);
+            chatroom.Login(userTwo.Nickname, userTwo.GroupID);
+            chatroom.WriteMessage(second);
+
+            chatroom.SetFilterAndSort(1, 0, true, "", "");
+            List<String> messages = chatroom.GetAllMessages();
+            int i = 0;
+            foreach (String mess in messages)
+            {
+                Assert.AreEqual(mess.Contains(test[i]), true);
+                i++;
+            }
+
+            chatroom.SetFilterAndSort(1, 0, false, "", "");
+            messages = chatroom.GetAllMessages();
+            i = 3;
+            foreach (String mess in messages)
+            {
+                Assert.AreEqual(mess.Contains(test[i]), true);
+                i--;
+            }
+
+        }
+
+        [TestMethod()]
+        public void TestSortByAll()
+        {
+            String One_first = "first message of userOne";
+            String One_second = "second message of userOne";
+            String Two_first = "first message of userTwo";
+            String Two_second = "second message of userTwo";
+            String Three_first = "first message of userThree";
+            String Three_second = "second message of userThree";
+            String Four_first = "first message of userFour";
+            String Four_second = "second message of userFour";
+            List<String> test = new List<String>(8)
+            {
+                Three_first, Three_second, Two_first, Two_second,
+                Four_first, Four_second, One_first, One_second
+                 
+            };
+            chatroom.RestartChatroom();
+
+            chatroom.Register(userThree.Nickname, userThree.GroupID);
+            chatroom.Login(userThree.Nickname, userThree.GroupID);
+            chatroom.WriteMessage(Three_first);
+            chatroom.Logout();
+            System.Threading.Thread.Sleep(2000);
+
+            chatroom.Register(userFour.Nickname, userFour.GroupID);
+            chatroom.Login(userFour.Nickname, userFour.GroupID);
+            chatroom.WriteMessage(Four_first);
+            chatroom.Logout();
+            System.Threading.Thread.Sleep(2000);
+
+            chatroom.Register(userOne.Nickname, userOne.GroupID);
+            chatroom.Login(userOne.Nickname, userOne.GroupID);
+            chatroom.WriteMessage(One_first);
+            chatroom.Logout();
+            System.Threading.Thread.Sleep(2000);
+
+            chatroom.Register(userTwo.Nickname, userTwo.GroupID);
+            chatroom.Login(userTwo.Nickname, userTwo.GroupID);
+            chatroom.WriteMessage(Two_first);
+            chatroom.Logout();
+            System.Threading.Thread.Sleep(2000);
+
+            chatroom.Register(userThree.Nickname, userThree.GroupID);
+            chatroom.Login(userThree.Nickname, userThree.GroupID);
+            chatroom.WriteMessage(Three_second);
+            chatroom.Logout();
+            System.Threading.Thread.Sleep(2000);
+
+            chatroom.Register(userFour.Nickname, userFour.GroupID);
+            chatroom.Login(userFour.Nickname, userFour.GroupID);
+            chatroom.WriteMessage(Four_second);
+            chatroom.Logout();
+            System.Threading.Thread.Sleep(2000);
+
+            chatroom.Register(userOne.Nickname, userOne.GroupID);
+            chatroom.Login(userOne.Nickname, userOne.GroupID);
+            chatroom.WriteMessage(One_second);
+            chatroom.Logout();
+            System.Threading.Thread.Sleep(2000);
+
+            chatroom.Register(userTwo.Nickname, userTwo.GroupID);
+            chatroom.Login(userTwo.Nickname, userTwo.GroupID);
+            chatroom.WriteMessage(Two_second);
+
+            chatroom.SetFilterAndSort(2, 0, true, "", "");
+            List<String> messagesAsc = chatroom.GetAllMessages();
+            int i = 0;
+            foreach (String mess in messagesAsc)
+            {
+                Assert.AreEqual(mess.Contains(test[i]), true);
+                i++;
+            }
+
+            chatroom.SetFilterAndSort(2, 0, false, "", "");
+            List<String> messagesDes = chatroom.GetAllMessages();
+            i = 7;
+            foreach (String mess in messagesDes)
+            {
+                Assert.AreEqual(mess.Contains(test[i]), true);
+                i--;
+            }
+
+        }
+
+
+        [TestMethod()]
+        public void TestFilter()
+        {
+            String first = "first message";
+            String second = "second message";
+            String third = "third message";
+            List<String> test = new List<String>(3)
+            {
+                first,
+                second,
+                third
+            };
+            chatroom.RestartChatroom();
+            chatroom.Register(userOne.Nickname, userOne.GroupID);
+            chatroom.Login(userOne.Nickname, userOne.GroupID);
+            chatroom.WriteMessage("my first message");
+            chatroom.WriteMessage("group 24 is the best");
+            chatroom.WriteMessage("100 final grade");
+            chatroom.Logout();
+
+            chatroom.Register(userTwo.Nickname, userTwo.GroupID);
+            chatroom.Login(userTwo.Nickname, userTwo.GroupID);
+            chatroom.WriteMessage("second message");
+            chatroom.WriteMessage("my message is not important");
+            chatroom.Logout();
+
+            chatroom.Register(userThree.Nickname, userThree.GroupID);
+            chatroom.Login(userThree.Nickname, userThree.GroupID);
+            chatroom.WriteMessage("third message");
+            
+            chatroom.SetFilterAndSort(0, 2, true, userOne.Nickname, "24");
+            List<String> messages = chatroom.GetAllMessages();
+            int i = 0;
+            foreach (String mess in messages)
+            {
+                Assert.AreEqual(mess, test[i]);
+                i++;
+            }
+
+            chatroom.SetFilterAndSort(0, 2, true, userOne.Nickname, "24");
+            messages = chatroom.GetAllMessages();
+            i = 3;
+            foreach (String mess in messages)
+            {
+                Assert.AreEqual(mess, test[i]);
+                i--;
+            }
+        }
+    }
+}
