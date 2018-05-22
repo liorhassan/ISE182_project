fed2f32290bb10365191e2c6625222d068d5d81d
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2018-05-22 18:22:53 +0300

    tests

--- ChatRoomApp/UnitTests/UnitTest1.cs
+++ ChatRoomApp/UnitTests/UnitTest1.cs
@@ -7,11 +7,11 @@ using System.Collections.Generic;
 
 namespace UnitTests
 {
+    // RestartChatroom clears the dictionaries so there will be no data inside
     [TestClass()]
     public class UnitTest1
     {
-        // create users
-        // RestartChatroom clears the dictionaries so there will be no data inside
+        // create users       
         User userOne = new User("userOne", "24");
         User userTwo = new User("userTwo", "5");
         User userThree = new User("userThree", "5");
@@ -22,9 +22,13 @@ namespace UnitTests
         {
             TestChatroom register = new TestChatroom();
             register.RestartChatroom();
+            // register the user for the first time
             Boolean firstR = register.Register(userOne.Nickname, userOne.GroupID);
+            // return true because user didn't exist in the chatroom before
             Assert.AreEqual(firstR, true);
+            // register the same user again
             Boolean secondR = register.Register(userOne.Nickname, userOne.GroupID);
+            // returns false because he is already registered 
             Assert.AreEqual(secondR, false);
         }
 
@@ -33,11 +37,15 @@ namespace UnitTests
         {
             TestChatroom login = new TestChatroom();
             login.RestartChatroom();
-            Console.WriteLine("after");
+            // register user
             login.Register(userTwo.Nickname, userTwo.GroupID);
+            // login an existing user
             Boolean firstL = login.Login(userTwo.Nickname, userTwo.GroupID);
+            // returns true because he exists
             Assert.AreEqual(firstL, true);
+            // login a nonexistent user
             Boolean secondL = login.Login("otheruser", "4");
+            // returns false because he doesn't exist
             Assert.AreEqual(secondL, false);
         }
 
@@ -46,11 +54,17 @@ namespace UnitTests
         {
             TestChatroom logout = new TestChatroom();
             logout.RestartChatroom();
+            // register user
             logout.Register(userOne.Nickname, userOne.GroupID);
+            // login user
             logout.Login(userOne.Nickname, userOne.GroupID);
+            // logout user
             Boolean firstlogout = logout.Logout();
+            // returns true because a user was loggenin before
             Assert.AreEqual(firstlogout, true);
+            // logout again
             Boolean secondlogout = logout.Logout();
+            // returns false because no user was loggedin
             Assert.AreEqual(secondlogout, false);
         }
 
@@ -59,12 +73,19 @@ namespace UnitTests
         {
             TestChatroom mes = new TestChatroom();
             mes.RestartChatroom();
+            // register user
             mes.Register(userOne.Nickname, userOne.GroupID);
+            // login user
             mes.Login(userOne.Nickname, userOne.GroupID);
+            // write a message
             int first = mes.WriteMessage("message");
+            // returns 1 because message is legal
             Assert.AreEqual(first, 1);
+            // create illegal message
             String s = new string('a', 151);
+            // write illegal message
             int second = mes.WriteMessage(s);
+            // retruns 1 because it's illegal and wasn't written
             Assert.AreEqual(second, -1);
         }
 
@@ -73,10 +94,12 @@ namespace UnitTests
         {
             TestChatroom sortbyname = new TestChatroom();
             sortbyname.RestartChatroom();
+            // strings of the 4 messages
             String first = "message of userOne";
             String second = "message of userTwo";
             String third = "message of userThree";
             String fourth = "message of userFour";
+            // order them in the order the sould be, alphabetically by username
             List<String> test = new List<String>(4)
             {
                 fourth,
@@ -84,7 +107,10 @@ namespace UnitTests
                 third,
                 second  
             };
-            
+            // register user
+            // login user
+            // write its' message
+            // logout user excect the last one
             sortbyname.Register(userThree.Nickname, userThree.GroupID);
             sortbyname.Login(userThree.Nickname, userThree.GroupID);
             sortbyname.WriteMessage(third);
@@ -104,18 +130,30 @@ namespace UnitTests
             sortbyname.Login(userTwo.Nickname, userTwo.GroupID);
             sortbyname.WriteMessage(second);
 
+            // set sort ascending
             sortbyname.SetFilterAndSort(1, 0, true, "", "");
+            // get the sorted messages ascending
             List<String> messages = sortbyname.GetAllMessages();
+            // initialize variable for iteration
             int i = 0;
+            // go through the messages and check that each message 
+            // contains the body of the message written in the beginning
+            // in the correct order
             foreach (String mess in messages)
             {
                 Assert.AreEqual(mess.Contains(test[i]), true);
                 i++;
             }
 
+            // set sort descending
             sortbyname.SetFilterAndSort(1, 0, false, "", "");
+            // get the sorted messages descending
             messages = sortbyname.GetAllMessages();
+            // initialize variable for iteration
             i = 3;
+            // go through the messages and check that each message 
+            // contains the body of the message written in the beginning
+            // in the correct order
             foreach (String mess in messages)
             {
                 Assert.AreEqual(mess.Contains(test[i]), true);
@@ -129,6 +167,7 @@ namespace UnitTests
         {
             TestChatroom sortbyall = new TestChatroom();
             sortbyall.RestartChatroom();
+            // strings of the 8 messages
             String One_first = "first message of userOne";
             String One_second = "second message of userOne";
             String Two_first = "first message of userTwo";
@@ -137,13 +176,19 @@ namespace UnitTests
             String Three_second = "second message of userThree";
             String Four_first = "first message of userFour";
             String Four_second = "second message of userFour";
+            // order them in the order the sould be, 
+            // first by group id, then username, then timestamp
             List<String> test = new List<String>(8)
             {
                 Three_first, Three_second, Two_first, Two_second,
                 Four_first, Four_second, One_first, One_second
                  
             };
-            
+            // register user
+            // login user
+            // write its' message
+            // logout user except the last 
+            // wait 2 seconds to make sure the time per image is different
             sortbyall.Register(userThree.Nickname, userThree.GroupID);
             sortbyall.Login(userThree.Nickname, userThree.GroupID);
             sortbyall.WriteMessage(Three_first);
@@ -190,18 +235,30 @@ namespace UnitTests
             sortbyall.Login(userTwo.Nickname, userTwo.GroupID);
             sortbyall.WriteMessage(Two_second);
 
+            // set sort ascending
             sortbyall.SetFilterAndSort(2, 0, true, "", "");
+            // get the sorted messages ascending
             List<String> messagesAsc = sortbyall.GetAllMessages();
+            // initialize variable for iteration
             int i = 0;
+            // go through the messages and check that each message 
+            // contains the body of the message written in the beginning
+            // in the correct order
             foreach (String mess in messagesAsc)
             {
                 Assert.AreEqual(mess.Contains(test[i]), true);
                 i++;
             }
 
+            // set sort descending
             sortbyall.SetFilterAndSort(2, 0, false, "", "");
+            // get the sorted messages descending
             List<String> messagesDes = sortbyall.GetAllMessages();
+            // initialize variable for iteration
             i = 7;
+            // go through the messages and check that each message 
+            // contains the body of the message written in the beginning
+            // in the correct order
             foreach (String mess in messagesDes)
             {
                 Assert.AreEqual(mess.Contains(test[i]), true);
@@ -216,9 +273,11 @@ namespace UnitTests
         {
             TestChatroom filter = new TestChatroom();
             filter.RestartChatroom();
+            // strings of the 3 messages by userOne
             String first = "first message";
             String second = "second message";
             String third = "third message";
+            // order them in the order the sould be, by timestamp 
             List<String> test = new List<String>(3)
             {
                 first,
@@ -226,6 +285,10 @@ namespace UnitTests
                 third
             };
             
+            // register user
+            // login user
+            // write a number of messages
+            // logout expect the last one
             filter.Register(userOne.Nickname, userOne.GroupID);
             filter.Login(userOne.Nickname, userOne.GroupID);
             filter.WriteMessage("my first message");
@@ -243,7 +306,7 @@ namespace UnitTests
             filter.Login(userThree.Nickname, userThree.GroupID);
             filter.WriteMessage("third message");
             
-            filter.SetFilterAndSort(0, 2, true, userOne.Nickname, "24");
+            filter.SetFilterAndSort(0, 2, true, userOne.Nickname, userOne.GroupID);
             List<String> messages = filter.GetAllMessages();
             int i = 0;
             foreach (String mess in messages)
@@ -252,7 +315,7 @@ namespace UnitTests
                 i++;
             }
 
-            filter.SetFilterAndSort(0, 2, true, userOne.Nickname, "24");
+            filter.SetFilterAndSort(0, 2, true, userOne.Nickname, userOne.GroupID);
             messages = filter.GetAllMessages();
             i = 3;
             foreach (String mess in messages)
