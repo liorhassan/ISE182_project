9f681dbd75ed4910fbec682c7b9678c41c3826ca
Ohad1 <37572368+Ohad1@users.noreply.github.com>
2018-05-29 20:18:53 +0300

    tests

---
+++ ChatRoomApp/SQL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace New
+{
+    class SQL
+    {
+        public static void Main(string[] args)        
+        {
+            string connetion_string = null;
+            string sql_query = null;
+            string server_address = "192.168.1.2,1010\\MS3";
+            string database_name = "MS3";
+            string user_name = "publicUser";
+            string password = "isANerd";
+
+            SqlConnection connection;
+            SqlCommand command;
+
+            connetion_string = $"Data Source={server_address};Initial Catalog={database_name };User ID={user_name};Password={password}";
+            connection = new SqlConnection(connetion_string);
+            SqlDataReader data_reader;
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("connected to: " + server_address);
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
